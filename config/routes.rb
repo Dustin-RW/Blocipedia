ae250ff6b1fbed4a1e5b7622410d020af77d5d5b
Dustin <dustinwaggoner@comcast.net>
2016-04-24 20:17:34 -0700

    modified home page to log in

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
 
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  root :to => "sessions#new"
 
   resources :users
   resources :sessions
