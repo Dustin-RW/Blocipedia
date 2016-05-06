51f55314437b0586469c8952900297534c2efb77
Dustin <dustinwaggoner@comcast.net>
2016-05-05 18:48:24 -0700

    api keys for stripe working and reverting to wikis with proper display message

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,6 +4,8 @@
    secret_key: ENV['SECRET_KEY']
  }
 
+ Stripe.api_key = ENV['SECRET_KEY']
+
  # Set our app-stored secret key with Stripe
  # Stripe.api_key = Rails.configuration.stripe[:secret_key]
  # STRIPE_PUBLIC_KEY = Rails.configuration.stripe[:publishable_key]
